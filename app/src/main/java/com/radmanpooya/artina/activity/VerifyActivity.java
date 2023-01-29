f478e7912dfaae4759d82784099ccc01f3488724
RadmanPooya <radmanpooya.co@gmail.com>
2023-01-29 21:05:19 +0330

    Complete some layout and java codes

--- app/src/main/java/com/radmanpooya/artina/activity/VerifyActivity.java
+++ app/src/main/java/com/radmanpooya/artina/activity/VerifyActivity.java
@@ -3,15 +3,20 @@ package com.radmanpooya.artina.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.radmanpooya.artina.R;
@@ -24,6 +29,10 @@ public class VerifyActivity extends AppCompatActivity {
     CardView timerCardView;
     Animation zoomInAnimation;
     CardView editMobileCardView;
+    EditText regNumber1;
+    EditText regNumber2;
+    EditText regNumber3;
+    EditText regNumber4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +41,7 @@ public class VerifyActivity extends AppCompatActivity {
 
         findViews();
         initialize();
-        onClicks();
+        listeners();
         setUpTimer();
     }
 
@@ -41,10 +50,14 @@ public class VerifyActivity extends AppCompatActivity {
         timerText = findViewById(R.id.timer_text);
         timerCardView = findViewById(R.id.timer_card_view);
         editMobileCardView = findViewById(R.id.edit_mobile_card_view);
+        regNumber1=findViewById(R.id.reg_number1);
+        regNumber2=findViewById(R.id.reg_number2);
+        regNumber3=findViewById(R.id.reg_number3);
+        regNumber4=findViewById(R.id.reg_number4);
 
     }
 
-    private void onClicks(){
+    private void listeners(){
         loginToApp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -59,6 +72,97 @@ public class VerifyActivity extends AppCompatActivity {
                 Intent intent = new Intent(VerifyActivity.this,SendOtpActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        regNumber1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(regNumber1.getText().toString().length()==1){
+                    regNumber2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        regNumber2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(regNumber2.getText().toString().length()==1){
+                    regNumber3.requestFocus();
+                }else if(regNumber2.getText().toString().length()==0){
+                    regNumber1.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        regNumber3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(regNumber3.getText().toString().length()==1){
+                    regNumber4.requestFocus();
+                }else if(regNumber3.getText().toString().length()==0){
+                    regNumber2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        regNumber4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(regNumber4.getText().toString().length()==1){
+
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
+                    if(regNumber1.getText().toString().matches("") || regNumber2.getText().toString().matches("") || regNumber3.getText().toString().matches("") || regNumber4.getText().toString().matches("")){
+
+                    }else{
+                        /*inputCode=regNumber1.getText().toString()+regNumber2.getText().toString()+regNumber3.getText().toString()+regNumber4.getText().toString();
+                        loginToKavinooTextView.setVisibility(View.INVISIBLE);
+                        progressBarLogin.setVisibility(View.VISIBLE);
+                        sendConfirm(inputCode,mobile);*/
+                    }
+
+                }else if(regNumber4.getText().toString().length()==0){
+                    regNumber3.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
     }
