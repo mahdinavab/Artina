0f45a6b654e44609e09da0acfcc119093003f191
Artina Application <artina.application@gmail.com>
2023-01-30 23:47:35 +0330

    Implement Learn List Activity

--- app/src/main/java/com/radmanpooya/artina/activity/LearnListActivity.java
+++ app/src/main/java/com/radmanpooya/artina/activity/LearnListActivity.java
@@ -1,13 +1,18 @@
 package com.radmanpooya.artina.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.view.ViewCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
@@ -21,14 +26,17 @@ import com.google.gson.reflect.TypeToken;
 import com.radmanpooya.artina.R;
 import com.radmanpooya.artina.adapter.education.category.CategoryAdapter;
 import com.radmanpooya.artina.adapter.education.learn.LearnAdapter;
+import com.radmanpooya.artina.adapter.market.sort.SortMarketAdapter;
 import com.radmanpooya.artina.api.Api;
 import com.radmanpooya.artina.api.Link;
 import com.radmanpooya.artina.model.education.category.CategoryResponseItem;
 import com.radmanpooya.artina.model.education.learn.LearnResponseItem;
+import com.radmanpooya.artina.model.market.sort.SortMarketModel;
 import com.radmanpooya.artina.util.NullStringToEmptyAdapterFactory;
 
 import org.aviran.cookiebar2.CookieBar;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,9 +47,14 @@ public class LearnListActivity extends AppCompatActivity {
     int categoryId;
     String categoryTitle;
 
-    RecyclerView recyclerLearn;
+    RecyclerView learnRecycler;
     LearnAdapter adapter;
     RecyclerView.LayoutManager layoutManager;
+
+    ConstraintLayout toolbar;
+    TextView toolbarTitle;
+    CardView backCardView;
+
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,35 +63,51 @@ public class LearnListActivity extends AppCompatActivity {
 
         findViews();
         initialize();
+        onClicks();
 
     }
 
     private void findViews(){
-
+        learnRecycler = findViewById(R.id.learn_recycler);
+        toolbar = findViewById(R.id.main_toolbar);
+        toolbarTitle = toolbar.findViewById(R.id.toolbar_title);
+        backCardView = toolbar.findViewById(R.id.back_card_view);
     }
 
     private void initialize(){
         categoryId = getIntent().getIntExtra("category_id",1);
         categoryTitle = getIntent().getStringExtra("category_title");
-        getLearnList(categoryTitle);
+        toolbarTitle.setText(categoryTitle);
+        getLearnList(categoryId+"");
     }
 
-
+    private void onClicks(){
+        backCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LearnListActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
     public void getLearnList(String catId){
+        Log.i("PPPPP",Link.LEARN_LIST+catId);
 
         final StringRequest learnReq = new StringRequest(Request.Method.GET, Link.LEARN_LIST+catId, new Response.Listener<String>() {
             @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onResponse(String response) {
 
+                Log.i("PPPPP",response);
                 try {
                     Gson gson = new GsonBuilder().registerTypeAdapterFactory(new NullStringToEmptyAdapterFactory()).create();
                     List<LearnResponseItem> learnResponseItems = gson.fromJson(response, new TypeToken<List<LearnResponseItem>>(){}.getType());
                     adapter=new LearnAdapter(LearnListActivity.this,learnResponseItems);
-                    recyclerLearn.setAdapter(adapter);
+                    learnRecycler.setAdapter(adapter);
                     layoutManager=new LinearLayoutManager(LearnListActivity.this);
-                    recyclerLearn.setLayoutManager(layoutManager);
-                    recyclerLearn.setHasFixedSize(true);
+                    learnRecycler.setLayoutManager(layoutManager);
+                    learnRecycler.setHasFixedSize(true);
 
                 }catch (Exception e){
 
@@ -113,4 +142,14 @@ public class LearnListActivity extends AppCompatActivity {
         Api.getInstance().addToRequestQueue(learnReq, "LEARNLIST");
 
     }
+
+    private void setSortItems(){
+        List<SortMarketModel> sortMarketModelList=new ArrayList<>();
+        sortMarketModelList.add(new SortMarketModel(0,"newer",R.drawable.newersorticon,"جدید ترین"));
+        sortMarketModelList.add(new SortMarketModel(1,"highestweight",R.drawable.highestweightsorticon,"بالا ترین وزن"));
+        sortMarketModelList.add(new SortMarketModel(2,"lowestweight",R.drawable.lowestweightsorticon,"پایین ترین وزن"));
+
+        
+    }
+
 }
